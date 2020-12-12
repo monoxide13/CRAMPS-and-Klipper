5dc86223924c9aa9f2fe9c89cd7736af45dc109e
monoxide13 <git@monoxide13.com>
2020-12-12 05:32:36 +0000

    Finished writing pin export script. All pins are now being exported. I have no idea if the device tree file is still needed to be loaded or not. Needs testing.

--- pin_config/CRAMPS.sh
+++ pin_config/CRAMPS.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#
+# The following comment section was taken from MachineKit, which I believe does a simular thing...
+
 # Any GPIO pins driven by the PRU need to have their direction set properly
 # here.  The PRU does not do any setup of the GPIO, it just yanks on the
 # pins and assumes you have the output enables configured already
@@ -8,54 +9,53 @@
 # Direct PRU inputs and outputs do not need to be configured here, the pin
 # mux setup (which is handled by the device tree overlay) should be all
 # the setup needed.
-pinArray=(
-"67 in" #X Min
-"66 in" #X Max
-"68 in" #Y Min
-"69 in" #Y Max
-"31 in" #Z Min
-"30 in" #Z Max
 
-#P8_11 low #FET 1 : Heated Bed
-#P8_12 low #X Dir Used in PRU
-#P8_13 low #X Step Used in PRU
-#P8_14 low #Y Dir Used in PRU
-#P8_15 low #Y Step Used in PRU
-#P8_16 high #eMMC Enable
-#P8_17 in #Estop In
-#P8_18 low #Z Dir Used in PRU
-#P8_19 low #Z Step Used in PRU
-#P8_20 default
-#P8_21 default
-#P8_22 low #Servo 4
-#P8_23 low #Servo 3
-#P8_24 low #Servo 2
-#P8_25 low #Servo 1
-#P8_26 high #Estop out
-#P9_11 in #Z Max
-#P9_12 low #E0 Dir Used in PRU
-#P9_13 in #Z Min
-#P9_14 high #Axis Enable, act. low
-#P9_15 low #FET 2 : E0
-#P9_16 low #E0 Step Used in PRU
-#P9_17 low #E1 Step Used in PRU
-#P9_18 low #E1 Dir Used in PRU
-#P9_21 low #FET 4 : E1
-#P9_22 low #FET 6
-#P9_23 low #Machine Power
-#P9_24 low #E2 Step
-#P9_25 low #LED
-#P9_26 low #E2 Dir
-#P9_27 low #FET 3 : E2
-#P9_28 spi #SPI CS0
-#P9_29 spi #SPI MISO
-#P9_30 spi #SPI MOSI
-#P9_31 spi #SPI SCLK
-#P9_41 low #FET 5
-#P9_42 spics #SPI CS1
-#P9_91 in #Reserved, con. to P9.41 
-#P9_92 in #Reserved, to P9.42
+# My understanding is that even though I set the pin mux in the device tree, I still need to tell linux that these pins are used.
+# As for the second part about PRU inputs and outpus, I have no idea what that means because I could not get anything working by only loading the device tree.
+
+######   DEFINE PINS   #####
+
+pinArray=(
+
+### PWR CTL ###
+"49 out" #Machine Power
+"50 out" #Motor Enable
+"61 out" #EStop switch out from software
+"27 in"  #EStop in from button
+
+### ENDSTOPS ###
+"67 in"  #X Min
+"66 in"  #X Max
+"68 in"  #Y Min
+"69 in"  #Y Max
+"31 in"  #Z Min
+"30 in"  #Z Max
+
+### STEP N DIRECTION ###
+"23 out" #X Step
+"44 out" #X Dir
+"47 out" #Y Step
+"26 out" #Y Dir
+"22 out" #Z Step
+"65 out" #Z Dir
+"51 out" #E0 Step
+"60 out" #E0 Dir
+"5 out"  #E1 Step
+"4 out"  #E1 Dir
+"15 out" #E2 Step
+"14 out" #E2 Dir
+
+### FETS ###
+"45 out" #1
+"48 out" #2
+"83 out" #3
+"3 out"  #4
+"20 out" #5
+"2 out"  #6
+
 )
+
+######   START SCRIPT   ######
 
 if [ "$EUID" -ne 0 ]; then
 	echo "Must run with sudo or as root"
